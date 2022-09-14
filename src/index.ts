5b4161e3599b0351ac5c526148b59296918ef2c2
sherif <sherif.elshamaaa@gmail.com>
2022-09-14 20:08:46 +0200

    Version 1

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from 'express'
+import * as dotenv from 'dotenv'
+import morgan from 'morgan'
+dotenv.config()
+const PORT = process.env.PORT
+const app: Application = express()
+
+app.use(morgan('dev'))
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to my API')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
+})
+
+export default app
